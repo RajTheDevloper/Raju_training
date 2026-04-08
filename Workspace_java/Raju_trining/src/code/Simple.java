f2c110780d67844e4c1fd5c95005e306af673542
Admin <Admin@CRVBLR00002>
2026-04-08 16:05:46 +0530

    something

---
+++ Workspace_java/Raju_trining/src/code/Simple.java
@@ -0,0 +1,10 @@
+package code;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		System.out.println("Raju is great");
+
+	}
+
+}
---
+++ Workspace_java/Raju_trining/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Raju_trining {
+}
